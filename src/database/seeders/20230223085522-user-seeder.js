6650703fd5a5fbee98d1b71a100a46958cba5cd9
Apriadi <apriadi4494@gmail.com>
2023-02-23 17:01:34 +0700

    config: added sequelize cli

---
+++ src/database/seeders/20230223085522-user-seeder.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          id: '9a10e4e9-7477-433a-acfd-c3a7d255c7d8',
+          email: 'admin@admin.com',
+          password:
+            '$2b$08$jwurorQhqBK7UpjJFEUXaOMONxw.ttpc7bDg96/Kad/b3j/nj5wry',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
