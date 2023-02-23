8a3f1d057c4beaf513cfd7a6a685f7e60b6ba0de
Apriadi <apriadi4494@gmail.com>
2023-02-23 13:29:24 +0700

    config: added configuration jwt passport

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,3 +6,7 @@ export const APP_NAME: string = process.env.APP_NAME;
 
 export const SWAGGER_URL: string = process.env.SWAGGER_URL;
 export const SWAGGER_VERSION: string = process.env.SWAGGER_VERSION;
+
+export const JWT_SECREET = process.env.JWT_SECREET;
+export const JWT_EXPIRES = process.env.JWT_EXPIRES;
+export const JWT_EXPIRES_REFRESH_TOKEN = process.env.JWT_EXPIRES_REFRESH_TOKEN;
